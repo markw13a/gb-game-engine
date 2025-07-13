30c697c784b7b7b27adac05bfcd930f9b96484c2
Mark Wood <markw13a@gmail.com>
2025-07-13 22:55:04 +0100

    Finished function to generate subgrid indices

--- src/utils/calculateRange.test.ts
+++ src/utils/calculateRange.test.ts
@@ -1,4 +1,4 @@
-import { calculateRange } from "./calculateRange";
+import { calculateIndices, calculateRange } from "./calculateRange";
 
 describe('calculateRange', () => {
     it('should calculate short range', () => {
@@ -17,5 +17,17 @@ describe('calculateRange', () => {
 
     it('should reject zero', () => {
         expect(() => calculateRange(0)).toThrowError();
+    });
+});
+
+describe('calculateIndices', () => {
+    it('should calculate sub-section indices', () => {
+        const indices = calculateIndices(3, 5, 12);
+
+        expect(indices).toEqual([6, 7, 8, 11, 12, 13, 16, 17, 18]);
+    });
+
+    it('should throw error if subsection larger than original grid', () => {
+        expect(() => calculateIndices(5, 3, 12)).toThrowError();
     });
 });
