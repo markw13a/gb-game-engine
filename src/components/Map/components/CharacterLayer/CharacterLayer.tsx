1640139e09f3615b637129512ee003144346d572
Mark Wood <markw13a@gmail.com>
2025-08-05 19:10:35 +0100

    Fixed issue with flickering character sprite on move

--- src/components/Map/components/CharacterLayer/CharacterLayer.tsx
+++ src/components/Map/components/CharacterLayer/CharacterLayer.tsx
@@ -1,6 +1,8 @@
+import { useEffect } from "react";
 import type { Direction, SpriteMap } from "../../../../types/types";
 
 import styles from "./CharacterLayer.module.css";
+import { preload } from "react-dom";
 
 type CharacterLayerProps = {
     direction: Direction;
@@ -10,6 +12,16 @@ type CharacterLayerProps = {
 
 export const CharacterLayer = ({ direction, moving, sprites }: CharacterLayerProps) => {
     const sprite = sprites[moving ? 'moving' : 'idle'][direction];
+
+    // Prevent flickering image effect by preloading sprites
+    useEffect(() => {
+        const idleSpriteUrls = Object.values(sprites.idle);
+        const movingSpriteUrls = Object.values(sprites.moving);
+
+        const spriteUrls = [...idleSpriteUrls, ...movingSpriteUrls];
+
+        spriteUrls.forEach(url => preload(url, { as: 'image' }));
+    }, [sprites])
 
     return (
         <div className={styles.container}>
