8c6731fbce9b80be4458da5672c7e766be711560
Mark Wood <markw13a@gmail.com>
2025-08-23 23:58:01 +0100

    Swap in map

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
 import "./App.css";
-// import { Map } from './components/Map/Map'
 import { MapEditor } from "./pages/MapEditor/MapEditor";
 import { TILE_DATA, TILES } from "./constants/symbols";
 import { map } from "./utils/symbolicMap/symbolicMap";
-// import { map } from './map/symbolicMap'
+import { Map } from "./pages/Pallet/Map";
 
 function App() {
 	return (
 		<>
-			{/* <Map map={map} /> */}
-			<MapEditor
+			<Map map={map} />
+			{/* <MapEditor
 				tileOptions={TILE_DATA}
 				tileSize="32px"
 				getTileSymbol={(tile) => tile.type}
 				getTileFromSymbol={(symbol) => TILES[symbol]}
 				getTileLabel={(tile) => tile.label}
 				getTileImgSrc={(tile) => tile.sprite}
-			/>
+			/> */}
 		</>
 	);
 }
