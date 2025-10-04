9fd0c0bb3e580bb7de6883ce81438819ad58d68b
Mark Wood <markw13a@gmail.com>
2025-10-04 13:00:45 +0100

    Added Gameboy Color title

--- src/pages/Pallet/components/ConsoleShell/ConsoleShell.tsx
+++ src/pages/Pallet/components/ConsoleShell/ConsoleShell.tsx
@@ -8,6 +8,17 @@ export const ConsoleShell = ({ children }: PropsWithChildren) => (
 	<div className={styles.console}>
 		<img aria-hidden className={styles.pikachu} src={pikachu} />
 		<img aria-hidden className={styles.pichu} src={pichu} />
+        <div className={styles.title}>
+            <span><i>GAME BOY</i></span>
+            {ColorText}
+        </div>
 		<div className={styles.screen}>{children}</div>
 	</div>
+);
+
+// Never needs to be rerendered
+const ColorText = (
+    <span>
+        {"COLOR".split('').map((letter, index) => <span className={styles.rainbow} key={index}>{letter}</span>)}
+    </span>
 );
