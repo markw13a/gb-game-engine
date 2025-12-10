4ef03ff66b38c405ed39d7e7d4a7913ee030bd4e
Mark Wood <markw13a@gmail.com>
2025-12-10 14:45:40 +0000

    Fix object sprite

--- src/pages/Pallet/constants/objects.ts
+++ src/pages/Pallet/constants/objects.ts
@@ -1,5 +1,8 @@
 import type { GameObject } from "@/lib/types/object";
 
+// HACK: Function would make more sense rather than tacking this on as/when
+const BASE_URL = import.meta.env.BASE_URL
+
 // These objects are tightly coupled with their maps because of the position fields
 // Maybe they should live together too?
 export const objects: GameObject[] = [
@@ -9,6 +12,6 @@ export const objects: GameObject[] = [
 		height: 2,
 		position: 300,
 		occupiedTiles: [300, 301, 340, 341],
-		sprite: "/sprites/objects/pokeball.png",
+		sprite: `${BASE_URL}/sprites/objects/pokeball.png`,
 	},
 ];
